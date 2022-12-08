1c111ed74e73684064eb088b2dea5b1b301133be
RanGlad12 <112420956+RanGlad12@users.noreply.github.com>
2022-12-08 06:11:41 -0500

    Got up to the point of training the classification model

--- deep_squat_model.py
+++ deep_squat_model.py
@@ -1,113 +1,36 @@
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
+'''
+Constructs an EfficientNet based Tensorflow model with the top layers removed.
+Added a Dense layer of 100 units and a Dense classifying layer of 2 units for the top layers
+Recieves:
+num_classes - the number of classes to classify
+img_height, img_width - the size of the image input to the network.
+Returns:
+model - Tensorflow model
+'''
 import tensorflow as tf
-import pandas as pd
-import os
-import shutil
+from keras.applications.efficientnet_v2 import EfficientNetV2B3
 
+def build_model(num_classes=2, img_height=299, img_width=299):
+  inputs = tf.keras.layers.Input(shape=(img_height, img_width, 3))
 
-# load dataset labels
-labels_path = 'Shallow_Squat_Error_Dataset/labels_shallow_depth.json'
-labels = pd.read_json(labels_path,  typ='dictionary', convert_axes=False ,convert_dates=False)
-labels = labels.to_frame()
-labels = labels.reset_index()
+  model = EfficientNetV2B3(include_top=False, input_tensor=inputs, weights='imagenet')
+  
+  # Freeze the pretrained weights
+  #model.trainable = False
 
-# load training, validation and test split IDs
-ids_path = 'Shallow_Squat_Error_Dataset/splits'
-train_ids_path =  ids_path + '/train_ids.json'
-val_ids_path = ids_path + '/val_ids.json'
-test_ids_path = ids_path + '/test_ids.json'
+  # Rebuild top
+  x = tf.keras.layers.GlobalAveragePooling2D(name="avg_pool")(model.output)
+  x = tf.keras.layers.BatchNormalization()(x)
 
-train_ids = pd.read_json(train_ids_path, dtype=False, convert_axes=False ,convert_dates=False)
-val_ids = pd.read_json(val_ids_path, dtype=False, convert_axes=False ,convert_dates=False)
-test_ids = pd.read_json(test_ids_path, dtype=False, convert_axes=False ,convert_dates=False)
+  top_dropout_rate = 0.2
+  x = tf.keras.layers.Dropout(top_dropout_rate, name="top_dropout")(x)
+  x = tf.keras.layers.Dense(100, activation='relu', name='pre_pred')(x)
+  outputs = tf.keras.layers.Dense(num_classes, activation="softmax", name="pred")(x)
 
-# Create train, val, test directories
-parent_dir = 'Shallow_Squat_Error_Dataset'
-src_dir = 'Shallow_Squat_Error_Dataset/crops_unaligned'
-train_img_path = os.path.join(parent_dir, 'Train')
-train_img_shallow = os.path.join(train_img_path, 'shallow')
-train_img_deep = os.path.join(train_img_path, 'deep')
-val_img_path = os.path.join(parent_dir, 'Valid')
-val_img_shallow = os.path.join(val_img_path, 'shallow')
-val_img_deep = os.path.join(val_img_path, 'deep')
-test_img_path = os.path.join(parent_dir, 'Test')
-test_img_deep = os.path.join(test_img_path, 'deep')
-test_img_shallow = os.path.join(test_img_path, 'shallow')
-
-try:
-    os.makedirs(train_img_path)
-    os.makedirs(train_img_shallow)
-    os.makedirs(train_img_deep)
-    os.makedirs(val_img_path)
-    os.makedirs(val_img_shallow)
-    os.makedirs(val_img_deep)
-    os.makedirs(test_img_path)
-    os.makedirs(test_img_deep)
-    os.makedirs(test_img_shallow)
-except:
-    print('Folders already exist')
-
-# Copy images to the appropriate folders but only if they exist in the train/val/test split
-filenames = os.listdir(src_dir)
-
-for file in filenames:
-  try:
-    file_label = labels.loc[labels["index"] == file[:-4]].values[0][1]
-  except:
-    continue
-
-  if file[:-4] in train_ids.values:
-    if file_label:
-      shutil.copy(os.path.join(src_dir, file), train_img_deep)
-    else:
-      shutil.copy(os.path.join(src_dir, file), train_img_shallow)
-  if file[:-4] in val_ids.values:
-    if file_label:
-      shutil.copy(os.path.join(src_dir, file), val_img_deep)
-    else:
-      shutil.copy(os.path.join(src_dir, file), val_img_shallow)
-  if file[:-4] in test_ids.values:
-    if file_label:
-      shutil.copy(os.path.join(src_dir, file), test_img_deep)
-    else:
-      shutil.copy(os.path.join(src_dir, file), test_img_shallow)
-
-# Get number of training examples
-num_train = 0
-filenames = os.listdir(train_img_shallow)
-for file in filenames:
-  num_train += 1
-filenames = os.listdir(train_img_deep)
-for file in filenames:
-  num_train += 1
-
-# Define image size and batch size
-batch_size = 32
-img_height = 299
-img_width = 299
-
-# Create image data generators
-train_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=0.0,
-                                                                width_shift_range=0.2,
-                                                                height_shift_range=0.2,
-                                                                horizontal_flip=True)
-val_datagen = tf.keras.preprocessing.image.ImageDataGenerator(rotation_range=0.0,
-                                                                width_shift_range=0.2,
-                                                                height_shift_range=0.2,
-                                                                horizontal_flip=True)
-
-train_generator = train_datagen.flow_from_directory(train_img_path, 
-                                                    target_size=(img_height, img_width),
-                                                    color_mode="rgb",
-                                                    batch_size=32,
-                                                    class_mode="categorical",
-                                                    shuffle=True,
-                                                    )
-val_generator = train_datagen.flow_from_directory(val_img_path, 
-                                                    target_size=(img_height, img_width),
-                                                    color_mode="rgb",
-                                                    batch_size=32,
-                                                    class_mode="categorical",
-                                                    shuffle=True)
+  # Compile
+  model = tf.keras.Model(inputs, outputs, name="EfficientNet")
+  optimizer = tf.keras.optimizers.Adam(learning_rate=1e-4)
+  model.compile(
+      optimizer=optimizer, loss="categorical_crossentropy", metrics=["accuracy"]
+  )
+  return model
