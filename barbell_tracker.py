4f810e66de8ae8cdc0d8e56c35bb878f61ed4a1f
RanGlad12 <112420956+RanGlad12@users.noreply.github.com>
2022-12-09 01:53:15 -0500

    Added gym barbell dataset from roboflow

--- barbell_tracker.py
+++ barbell_tracker.py
@@ -1,3 +1,11 @@
 import torch
 import os
 from IPython.display import Image, clear_output  # to display images
+
+print(f"Setup complete. Using torch {torch.__version__} ({torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'})")
+
+# Import barbell dataset from Roboflow
+from roboflow import Roboflow
+rf = Roboflow(api_key="t6tj7mQYwRntDdaHocGQ")
+project = rf.workspace("ecnu").project("gym_barbell")
+dataset = project.version(1).download("yolov5")
