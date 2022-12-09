fdf61db06e5c5fd81f395bd78c126d32ee3668d3
RanGlad12 <112420956+RanGlad12@users.noreply.github.com>
2022-12-09 04:22:51 -0500

    made yolo run locally

--- barbell_tracker.py
+++ barbell_tracker.py
@@ -4,11 +4,12 @@ from IPython.display import Image, clear_output  # to display images
 
 print(f"Setup complete. Using torch {torch.__version__} ({torch.cuda.get_device_properties(0).name if torch.cuda.is_available() else 'CPU'})")
 
-# Import barbell dataset from Roboflow
-from roboflow import Roboflow
-#rf = Roboflow(api_key="t6tj7mQYwRntDdaHocGQ")
-#project = rf.workspace("ecnu").project("gym_barbell")
-#dataset = project.version(1).download("yolov5")
+train = True
+if train:
+    print('Start training yolov5')
+    os.chdir('yolov5')
+    os.system('python train.py --img 416 --batch 16 --epochs 150 --data ../Gym_barbell-1/data.yaml --weights yolov5s.pt --cache --cfg ../custom_yolov5s.yaml')
+    os.chdir('..')    
 
 
 
