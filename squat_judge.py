e3640014d0575f89dbc6ceb084d80d10e968f836
RanGlad12 <112420956+RanGlad12@users.noreply.github.com>
2022-12-10 07:54:34 -0500

    trying to fix yolo folder

--- squat_judge.py
+++ squat_judge.py
@@ -21,6 +21,10 @@ Note that to train the classifier you need access to a deep/shallow
 squat database, as I cannot provide the database I have used
 due to a user agreement and privacy concerns.
 '''
+# Install yolov5
+os.system('git clone https://github.com/ultralytics/yolov5')
+os.chdir('yolov5')
+os.system('pip install -qr requirements.txt')
 # change to True if you want to train
 #  the deep squat image classifier
 train_classifier = False
